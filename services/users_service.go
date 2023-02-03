5092c975614aeb6f17c3d0e22a63214a784e3e6b
imaarov <imanatarof@gmail.com>
2023-02-03 20:17:01 +0330

    add create service base function for storing logic

--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"github.com/imaarov/bookstore_microservice/domain/users"
+	"github.com/imaarov/bookstore_microservice/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return &user, nil
+}
